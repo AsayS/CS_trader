8b67b1b83da856a4aa123de8c373c841950ba990
AsayS <gvido.ko@gmail.com>
2023-12-15 01:40:28 +0200

    tava mamma

--- item_search/buff_id.py
+++ item_search/buff_id.py
@@ -1,30 +1,40 @@
 import webbrowser
+import win32clipboard
+import keyboard
+import time
+
+def open_browser(link):
+    webbrowser.open_new_tab("https://buff.163.com/goods/" + link)
+
 
 
 # Create dict from txt file
 d = {}
-with open("item_search/buffids.txt", "r") as fp:
-    lines = fp.readlines()
+with open(r"buffids.txt", "r", encoding="utf8") as f:
+    lines = f.readlines()
 for line in lines:
     key, value = line.split(";")
     d[value] = key
 
-
-search_for = "AK-47 | Bloodsport (Factory New)"
-usp = "USP-S | Overgrowth"
-val = 34444
-
-
 all_found = []
-# Search for buff value fomr skin name
-for key, value in d.items():
-    if usp in key:
-        all_found.append(value)
 
+while not keyboard.is_pressed("f12"):
+    if keyboard.is_pressed("f9"):
+        win32clipboard.OpenClipboard()
+        search = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
 
-print(all_found[0])
-print(type(all_found))
+        all_found.clear()
+        # Search for buff value fomr skin name
+        for key, value in d.items():
+            if search in key:
+                all_found.append(value)
+                time.sleep(0.1)
+                webbrowser.open_new_tab("https://buff.163.com/goods/" + all_found[0])
+                break
+    # if keyboard.is_pressed("f10"):
+    #     time.sleep(0.1)
+    #     open_browser(all_found[0])
+    
 
-webbrowser.open_new_tab("https://buff.163.com/goods/" + str(all_found[0]))
-webbrowser.get('chrome %s').open_new_tab(
-    "https://buff.163.com/goods/" + str(all_found[0]))
+# webbrowser.open_new_tab("https://buff.163.com/goods/" + all_found[0])
